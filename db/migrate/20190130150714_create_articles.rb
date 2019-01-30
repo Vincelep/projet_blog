2f4354f396f0f3f7fb5b8ed024c82c59c1748cfe
vincelep <vincent.leparoux@gmail.com>
2019-01-30 17:45:53 +0100

    first commit

---
+++ db/migrate/20190130150714_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.belongs_to :user, index: true
+      t.string :title
+      t.text :content
+      t.integer :user_id
+      t.integer :category_id
+      t.timestamps
+    end
+  end
+end
